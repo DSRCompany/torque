319364cd85caea82ef575d0c121b10cc69479a00
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-company.com>
2013-08-24 20:24:06 +0400

    Updated formatting and comments.

--- src/include/Message.hpp
+++ src/include/Message.hpp
@@ -15,53 +15,80 @@
 
 namespace TrqJson {
 
+  /**
+   * Base class for all Torque Json messages.
+   * The class is designed to use with ZeroMQ messages and provides two main public methods for
+   * this: write() that serializes this message data to the given string and deleteString() routine
+   * that deallocates the string and have to be passed to ZMQ message on initialization state.
+   */
   class Message
     {
 
-  private:
-    std::string momId;
-    
-    /**
-     * Generates UUID.
-     * NOTE: current implementation just generates random sequence. For better security libuuid should
-     * be used.
-     * @return random string in UUID format.
-     */
-    static std::string generateUuid();
+    private:
 
-    /**
-     * Gets the current timestamp represented in ISO 8601 format in local time with timezone offset.
-     * @return a string containing the timestamp.
-     */
-    static std::string getIso8601Time();
+      /**
+       * The ID of the MOM owning this message.
+       */
+      std::string momId;
 
-    /**
-     * Create standard for all Json messages header.
-     * @param message_type the type of the message.
-     * @return JsonCPP object that could be updated with other specific information.
-     */
-    void generateHeader(Json::Value &root);
+      /**
+       * Generates UUID.
+       * NOTE: current implementation just generates random sequence. For better security libuuid
+       * should be used.
+       * @return random string in UUID format.
+       */
+      static std::string generateUuid();
 
-  protected:
-    virtual void generateBody(Json::Value &messageBody) = 0;
-    virtual std::string getMessageType() = 0;
+      /**
+       * Gets the current timestamp represented in ISO 8601 format in local time with timezone
+       * offset.
+       * @return a string containing the timestamp.
+       */
+      static std::string getIso8601Time();
 
-  public:
-    void setMomId(std::string momId);
+      /**
+       * Create standard for all Json messages header.
+       * @param message_type the type of the message.
+       * @return JsonCPP object that could be updated with other specific information.
+       */
+      void generateHeader(Json::Value &root);
 
-    /**
-     * Dump all collected Json status messages into a character buffer.
-     * @return heap char array that have to be deallocated with free().
-     */
-    void write(std::string &out);
+    protected:
 
-    /**
-     * Deallocate character buffer.
-     * The function is designed to be passed to zmq_msg_init_data().
-     * @param data the buffer to be deallocated.
-     * @param hint isn't used.
-     */
-    static void deleteString(void *data, void *hint);
+      /**
+       * The methond have to be implemented by specific implementations. The method should fill the
+       * given Json::Value object with corresponding message data.
+       * @param messageBody message body value.
+       */
+      virtual void generateBody(Json::Value &messageBody) = 0;
+
+      /**
+       * The method have to be implemented by specific implementations. The method should return 
+       * message type string like "status", "event" and so on.
+       */
+      virtual std::string getMessageType() = 0;
+
+    public:
+
+      /**
+       * Set the ID of MOM owning the message. It's needed for "senderId" value.
+       */
+      void setMomId(std::string momId);
+
+      /**
+       * Dump all collected Json status messages into a character buffer.
+       * @return heap char array that have to be deallocated with free().
+       */
+      void write(std::string &out);
+
+      /**
+       * Deallocate character buffer.
+       * The function is designed to be passed to zmq_msg_init_data().
+       * @param data the buffer to be deallocated.
+       * @param hint isn't used.
+       */
+      static void deleteString(void *data, void *hint);
+
     }; /* class Message */
 
 } /* namespace TrqJson */
--- src/include/MomStatusMessage.hpp
+++ src/include/MomStatusMessage.hpp
@@ -9,30 +9,66 @@
 
 namespace TrqJson {
 
+  /**
+   * Message class implementation specific for MOM Status messages.
+   */
   class MomStatusMessage : public Message
     {
-  private:
-    std::map<std::string, Json::Value> statusMap;
 
-  protected:
-    void generateBody(Json::Value &messageBody);
-    std::string getMessageType();
+    private:
 
-  public:
-    /**
-     * Parse and handle mom status message from the given buffer.
-     * @param sz data buffer size.
-     * @param data message data buffer.
-     * @return 0 if succeeded or -1 otherwise.
-     */
-    int readMergeJsonStatuses(const size_t size, const char *data);
+      /**
+       * MOM statuses container. This keeps all received statuses and this MOM status.
+       * It have to be cleared after the message is sent.
+       */
+      std::map<std::string, Json::Value> statusMap;
 
-    /**
-     * Update this MOM status message in the global statuses map with the values from the given
-     * status_strings dynamic string.
-     * @param status_strings dynamic string containing this MOM status.
-     */
-    void readMergeStringStatus(const char *nodeId, const char *statusStrings);
+      /**
+       * Helper function that handles GPU status part of the status message.
+       * @param statusStrings dynamic status strings. The first one have to be <gpu_status>
+       * @param status value where GPU status should be added.
+       * @return the pointer to the last string in the GPU status that is </gpu_status>
+       */
+      const char *readStringGpuStatus(const char *statusStrings, Json::Value &status);
+
+      /**
+       * Helper function that handles MIC status part of the status message.
+       * @param statusStrings dynamic status strings. The first one have to be <mic_status>
+       * @param status value where MIC status should be added.
+       * @return the pointer to the last string in the MIC status that is </mic_status>
+       */
+      const char *readStringMicStatus(const char *statusStrings, Json::Value &status);
+
+    protected:
+
+      /**
+       * Put all collected statuses to the given Json Value.
+       * @param messageBody the value to be updated
+       */
+      void generateBody(Json::Value &messageBody);
+
+      /**
+       * Return the message type. It's always "status" string.
+       * @return this message type.
+       */
+      std::string getMessageType();
+
+    public:
+
+      /**
+       * Parse and handle mom status message from the given buffer.
+       * @param sz data buffer size.
+       * @param data message data buffer.
+       * @return 0 if succeeded or -1 otherwise.
+       */
+      int readMergeJsonStatuses(const size_t size, const char *data);
+
+      /**
+       * Update this MOM status message in the global statuses map with the values from the given
+       * status_strings dynamic string.
+       * @param status_strings dynamic string containing this MOM status.
+       */
+      void readMergeStringStatus(const char *nodeId, const char *statusStrings);
 
     }; /* class MomStatusMessage */
 
